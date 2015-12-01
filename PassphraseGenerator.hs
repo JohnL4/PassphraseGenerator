7688c0543c3e384c008998a870ab1a00d743715c
John Lusk <john-git@how-hard-can-it-be.com>
2015-11-30 19:15:36 -0500

    Performance improvement:  Use a set rather than a list to name the parts of speech we're interested in.

--- PassphraseGenerator.hs
+++ PassphraseGenerator.hs
@@ -7,12 +7,13 @@ module PassphraseGenerator where
 import Data.Char (toLower)
 import qualified Data.Map.Strict as Map
 import Data.List.Split (splitOn)
+import Data.Set (Set, fromList, member)
 import Text.Regex.TDFA
 
 -- | The parts of speech (POS) we want to restrict our counting to.  We use "" to also pick up those
 -- | ngrams that haven't been tagged with POS at all.
-significantNgramRoles :: [String]
-significantNgramRoles = ["", "NOUN","VERB","ADJ","ADV"]
+significantNgramRoles :: Set String -- Balanced binary tree; more efficient than [String], which is implemented as a linked list.
+significantNgramRoles = fromList ["NOUN","VERB","ADJ","ADV"]
 
 romanNumeral :: Regex
 romanNumeral = makeRegexOpts defaultCompOpt{multiline=False} defaultExecOpt "^[ivxlc]+$"
@@ -41,8 +42,8 @@ wordCounts aYear (aLine:restLines) !aMap =
       ngramMatchCount = read (fields!!2) :: Int
   in if (year < aYear)
         -- || (ngramRole == "DET") -- Skip "determiners" (words like "a", "an", "the")
-        || (not (elem ngramRole significantNgramRoles))
         || ((length ngram) < 3)  -- Two-letter words
+        || (not (member ngramRole significantNgramRoles))
         || (match romanNumeral ngram) -- Also need to eliminate Roman numerals: [ivxlc]+ (not using
                                       -- "m" because "mix" is a real word)
      then (wordCounts aYear restLines aMap)
